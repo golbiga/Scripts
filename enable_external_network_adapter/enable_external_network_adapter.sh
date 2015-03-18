ef2c4c80c4ceae2f673d91e54a88c9600bbcc0c6
Allen Golbig <golbiga@gmail.com>
2015-03-18 13:20:30 -0400

    No grep before awk & added MacBookPro12

--- enable_external_network_adapter/enable_external_network_adapter.sh
+++ enable_external_network_adapter/enable_external_network_adapter.sh
@@ -2,16 +2,16 @@
 # Checks to see if the Mac is either a MacBook Pro Retina or MacBook Air
 # If it's either of these machines, the script will then check for External Network Adapters
 # If either adapter is present, it will add the adapter to network services
-# Resolves an issue with USB & Thunderbolt Ethernet adapters with DeployStudio 1.6.3
+# Resolves an issue with USB & Thunderbolt Ethernet adapters with DeployStudio
 
-mbpr=`system_profiler SPHardwareDataType | grep "Model Identifier" | awk '{print $3}' | cut -f1 -d ","`
-mba=`system_profiler SPHardwareDataType | grep "Model Identifier" | awk '{print $3}' | cut -c-10`
+mbpr=`system_profiler SPHardwareDataType | awk '/Model Identifier/{print $3}' | cut -f1 -d ","`
+mba=`system_profiler SPHardwareDataType | awk '/Model Identifier/{print $3}' | cut -c-10`
 usbAdapter=`/usr/sbin/networksetup -listallhardwareports | grep "Hardware Port: USB Ethernet"`
 tbAdapter=`/usr/sbin/networksetup -listallhardwareports | grep "Hardware Port: Thunderbolt Ethernet"`
 
 /usr/sbin/networksetup -detectnewhardware
 
-if [ $mbpr = "MacBookPro10" -o $mbpr = "MacBookPro11" ]; then
+if [[ $mbpr = "MacBookPro10" ]] || [[ $mbpr = "MacBookPro11" ]] || [[ $mbpr = "MacBookPro12" ]]; then
 	if [ "$usbAdapter" != "" ]; then
 		/usr/sbin/networksetup -createnetworkservice USB\ Ethernet 'USB Ethernet'
 		echo "USB Ethernet added to Network Services"
